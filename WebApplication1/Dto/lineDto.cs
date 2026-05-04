283afd5f8bad6c37bc60ca4dedfb415621f5916e
Ahmad Akhras <a.akhras@penguinin.com>
2026-05-04 15:09:13 +0300

    lineDeto

---
+++ WebApplication1/Dto/lineDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Dto
+{
+    public class LineDto
+    {
+       
+
+        public int? id { get; set; }
+        public int? first_node_id { get; set; }
+        public int? second_node_id { get; set; }
+        public bool? is_two_way { get; set; }
+
+    }
+}
